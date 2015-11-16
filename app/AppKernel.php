351435d7b282a650a31f7e53cb75940a9ef92ac1
Daniele Murroni <daniele.murroni@gmail.com>
2015-11-16 17:01:24 +0000

    removed propel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Propel\PropelBundle\PropelBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Bones\CoreBundle\BonesCoreBundle(),
             new Bones\UserBundle\BonesUserBundle(),
