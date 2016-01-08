226dbb23c2d83ca9946fca0065f6f76b0b3b2335
Daniele Murroni <daniele.murroni@gmail.com>
2016-01-08 14:11:37 +0000

    asdasd

---
+++ src/App/AppBundle/Entity/LearningHubAccessPage.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * LearningHubAccessPage
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class LearningHubAccessPage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="introduction", type="text")
+     */
+    private $introduction;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="button_text", type="string", length=255)
+     */
+    private $buttonText;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=10)
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return LearningHubAccessPage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set introduction
+     *
+     * @param string $introduction
+     *
+     * @return LearningHubAccessPage
+     */
+    public function setIntroduction($introduction)
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    /**
+     * Get introduction
+     *
+     * @return string
+     */
+    public function getIntroduction()
+    {
+        return $this->introduction;
+    }
+
+    /**
+     * Set buttonText
+     *
+     * @param string $buttonText
+     *
+     * @return LearningHubAccessPage
+     */
+    public function setButtonText($buttonText)
+    {
+        $this->buttonText = $buttonText;
+
+        return $this;
+    }
+
+    /**
+     * Get buttonText
+     *
+     * @return string
+     */
+    public function getButtonText()
+    {
+        return $this->buttonText;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return LearningHubAccessPage
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
---
+++ src/App/AppBundle/Form/LearningHubAccessPageType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LearningHubAccessPageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('introduction')
+            ->add('buttonText')
+            ->add('status')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\AppBundle\Entity\LearningHubAccessPage'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_appbundle_learninghubaccesspage';
+    }
+}
