69a1ee4e0b8c930e111bbceba3204221cf0ba565
Daniele Murroni <daniele.murroni@gmail.com>
2016-02-23 15:02:07 +0000

    migration bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Bones\CoreBundle\BonesCoreBundle(),
             new Bones\UserBundle\BonesUserBundle(),
             new App\AppBundle\AppAppBundle(),
